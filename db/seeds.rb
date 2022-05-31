23688433a2eb574f7f694b203cf410f01b83061f
tYohannes <teklegy@gmail.com>
2022-05-31 13:31:25 +0300

    Create Message model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(greeting: 'Hi')
+Message.create(greeting: 'Hello')
+Message.create(greeting: 'How are you!')
+Message.create(greeting: "What's up?")
+Message.create(greeting: 'Peace')
